9660ef9c0c9b4d80cc6531f7da1eaed2f3a2b3e2
Tarun Verghis <tarun.verghis@gmail.com>
2022-01-03 01:44:53 -0800

    Prioritize marking correct letters
    Closes #1

--- src/row/mod.rs
+++ src/row/mod.rs
@@ -36,6 +36,9 @@ impl Row {
     }
 
     pub fn new_guess(guess: &str, target: &str) -> Self {
+        // We make a big assumption in this function that both `guess` and
+        // `target` are of the same length!
+
         let mut char_counts = HashMap::new();
 
         for c in target.chars() {
@@ -43,31 +46,44 @@ impl Row {
             *entry += 1;
         }
 
-        let letters = guess
-            .chars()
-            .zip(target.chars())
-            .map(|(g, t)| match char_counts.get_mut(&g) {
-                None => Letter::new(g, LetterState::RevealedIncorrect),
+        let guess_chars = guess.as_bytes();
+        let target_chars = target.as_bytes();
+
+        let mut letters = guess
+            .as_bytes()
+            .iter()
+            .map(|&c| Letter::new(c as char, LetterState::RevealedIncorrect))
+            .collect::<Vec<_>>();
+
+        // First, mark every character that is already in the correct spot.
+        for (idx, &target_c) in target_chars.iter().enumerate() {
+            if target_c == guess_chars[idx] {
+                letters[idx].state = LetterState::RevealedCorrect;
+                *char_counts.get_mut(&(target_c as char)).unwrap() -= 1;
+            }
+        }
+
+        // Then, for every other character, check if it's shifted or incorrect.
+        for (idx, &guess_c) in guess_chars.iter().enumerate() {
+            // Skip the ones we've already marked as correct.
+            if let LetterState::RevealedCorrect = letters[idx].state {
+                continue;
+            }
+
+            letters[idx].state = match char_counts.get_mut(&(guess_c as char)) {
+                None => LetterState::RevealedIncorrect,
                 Some(count) => {
                     if *count < 1 {
-                        return Letter::new(g, LetterState::RevealedIncorrect);
-                    }
-
-                    let state = if t == g {
-                        LetterState::RevealedCorrect
+                        LetterState::RevealedIncorrect
                     } else {
+                        *count -= 1;
                         LetterState::RevealedShifted
-                    };
-
-                    *count -= 1;
-
-                    Letter::new(g, state)
+                    }
                 }
-            })
-            .collect::<Vec<_>>()
-            .as_slice()
-            .try_into()
-            .unwrap();
+            }
+        }
+
+        let letters = letters.try_into().unwrap();
 
         Row {
             letters,
