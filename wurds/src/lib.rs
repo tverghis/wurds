76cbeb860b02900905085266e4b179ff3049ba46
Tarun Verghis <tarun.verghis@gmail.com>
2022-01-03 23:47:57 -0800

    Increase max guesses to 6 again

--- wurds/src/lib.rs
+++ wurds/src/lib.rs
@@ -2,7 +2,7 @@ pub mod game_state;
 pub mod row;
 
 /// The maximum number of guesses afforded to the user.
-pub const MAX_GUESSES: usize = 5;
+pub const MAX_GUESSES: usize = 6;
 
 /// Number of letters in the puzzle word (exact, not maximum).
 pub const WORD_SIZE: usize = 5;
